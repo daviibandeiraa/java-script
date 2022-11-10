8cbe945ed18124dd7ba1b18d4424df40114ada75
Davi Barbosa Bandeira <daviibandeiraa@gmail.com>
2022-11-09 21:29:01 -0300

    aula e java script (tudo no console)

---
+++ app.js
@@ -0,0 +1,49 @@
+console.log("Olá Mundo")
+console.log(2+2)
+console.log(2*2)
+console.log(10/2)
+console.log(10-2)
+/* comentario de 
+varias linhas */
+
+// comentario de uma linha//
+
+/*variaveis*/
+
+/* - váriavel é um espaço reservado na memória para armazenar
+um valor
+- toda variavel tem um nome e um tipo*/
+
+let nome = 'Davi'
+let idade = 18
+let nota1 = 10
+let fumante = true
+
+//nome
+
+console.log(nome);
+console.log(idade, nota1, fumante)
+console.log('Sua nota é ' + nota1 )
+
+/* Tipos */
+console.log( typeof nome) // 'Davi' é string
+console.log( typeof idade) // 18 é number
+console.log( typeof nota1 ) // 10 é number
+console.log( typeof fumante) // true é boolean
+
+let veiculos = ('carro', 'moto', 'avião')
+console.log(veiculos)
+console.log( typeof veiculos )
+
+let cliente  = {
+    nome: 'Davi',
+    idade: 18
+}
+console.log(cliente)
+console.log( typeof cliente)
+
+let salvar = function(){
+
+}
+console.log(typeof salvar)
+
